6315193d14aca7cebd8b4dfe4d1d654c92e004d6
Gagelbob <tobi.kumm@t-online.de>
2020-02-27 02:16:32 +0100

    es gibt hoffentlich ein menu und plugs
    Habe ein noch nicht ganz fertiges menu gebaut und ein text plug installiert mit dem man tolle dinge machen kann un der besser als der vanilla kack ist.

---
+++ Livelauf/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+
+    public void LadeSimulation()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void VerlasseSimulation()
+    {
+        Debug.Log("Simulation wird verlassen.")
+        Application.Quit();
+
+    }
+
+}
