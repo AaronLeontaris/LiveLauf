333e83f2b5f79b56c3fde1311dcd813ed478b020
AaronLeontaris <aaron.leontaris@web.de>
2020-02-28 22:03:15 +0100

    mein TP funzt aber auch nicht

---
+++ Livelauf/Assets/Scripts/rolandsTP.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rolandsTP : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public CharacterController thePLayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        thePLayer.transform.position = teleportTarget.transform.position;
+    }
+}
