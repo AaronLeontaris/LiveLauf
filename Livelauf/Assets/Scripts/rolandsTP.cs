0e567ef5269bc1af47cda61d9203308c3edcb9c4
AaronLeontaris <aaron.leontaris@web.de>
2020-03-02 16:10:27 +0100

    Revert "DIveses"
    This reverts commit ca7da39837deee6df4f0a051ecb1e9ff18842466.

--- Livelauf/Assets/Scripts/rolandsTP.cs
+++ Livelauf/Assets/Scripts/rolandsTP.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class rolandsTP : MonoBehaviour
 {
     public Transform teleportTarget;
-    public GameObject thePLayer;
+    public CharacterController thePLayer;
 
     private void OnTriggerEnter(Collider other)
     {
